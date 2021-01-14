c405a1abc5b5a96ddf14c675e9e76dc5dfc85424
Luckyleqi <leqizeng00@gmail.com>
2021-01-14 11:47:02 -0800

    2 round practice

--- models.py
+++ models.py
@@ -45,7 +45,7 @@ def parse_config(config):
 class Constants(BaseConstants):
     name_in_url = 'ri_call_market'
     players_per_group = None
-    num_rounds = 4
+    num_rounds = 2
     def total_rounds(self):
         return self.config.get('total_rounds')
     def round_number(self):
