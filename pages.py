ef6cf80d9cdbc71d93bcc47606bd7319a917239d
Luckyleqi <leqizeng00@gmail.com>
2021-01-14 13:38:31 -0800

    time limit change:

--- pages.py
+++ pages.py
@@ -24,7 +24,7 @@ class block_page(Page):
         }
 
 class MainPage(Page):
-    timeout_seconds = 180
+    timeout_seconds = 120
     form_model = 'player'
     form_fields = ['width', 'cost', 'm_low', 'm_high', 'low_val', 'high_val', 'bid_price', 'ask_price']
 
